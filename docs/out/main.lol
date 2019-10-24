54fcc2d62da9c88a68babdcc6375f44f3419c90a
Damian Koper <kopernickk@gmail.com>
2019-10-24 21:46:02 +0200

    Sprawko

---
+++ docs/out/main.lol
@@ -0,0 +1,4 @@
+\contentsline {lstlisting}{\numberline {1}Bazowy program wy\IeC {\'s}wietlaj\IeC {\k a}cy czarne okno.}{3}{lstlisting.1}% 
+\contentsline {lstlisting}{\numberline {2}Funkcja rysuj\IeC {\k a}ca dywan Sierpi\IeC {\'n}skiego rekurencyjnie. Pomini\IeC {\k e}to niekt\IeC {\'o}re wywo\IeC {\l }ania.}{5}{lstlisting.2}% 
+\contentsline {lstlisting}{\numberline {3}Struktura danych przechowuj\IeC {\k a}ca aktualny stan rysowania.}{6}{lstlisting.3}% 
+\contentsline {lstlisting}{\numberline {4}Funkcja rysuj\IeC {\k a}ca dywan Sierpi\IeC {\'n}skiego iteracyjnie. Pomini\IeC {\k e}to niekt\IeC {\'o}re wywo\IeC {\l }ania.}{6}{lstlisting.4}% 
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,114 @@
 #include <GL/glut.h>
-#include "XD.hpp"
+#include <time.h>
+#include <random>
+#include <queue>
+using namespace std;
+
+unsigned char randChar()
+{
+  return rand() % 256;
+}
+
+void randColor()
+{
+  glColor3ub(0, randChar(), 0);
+}
+
+void rect(float dx, float dy, float a)
+{
+  glBegin(GL_TRIANGLES);
+  randColor();
+  glVertex2f(-a / 2 + dx, -a / 2 + dy);
+  randColor();
+  glVertex2f(a / 2 + dx, -a / 2 + dy);
+  randColor();
+  glVertex2f(-a / 2 + dx, a / 2 + dy);
+
+  randColor();
+  glVertex2f(a / 2 + dx, a / 2 + dy);
+  randColor();
+  glVertex2f(-a / 2 + dx, a / 2 + dy);
+  randColor();
+  glVertex2f(a / 2 + dx, -a / 2 + dy);
+  glEnd();
+}
+
+
+
+void carpet(int levels, float a, float distort = 0, float dx = 0, float dy = 0)
+{
+  if (levels != 0)
+  {
+    a = a / 3;
+    carpet(levels - 1, a, distort, dx - a, dy - a);
+    carpet(levels - 1, a, distort, dx, dy - a);
+    carpet(levels - 1, a, distort, dx + a, dy - a);
+    carpet(levels - 1, a, distort, dx - a, dy);
+    carpet(levels - 1, a, distort, dx + a, dy);
+    carpet(levels - 1, a, distort, dx - a, dy + a);
+    carpet(levels - 1, a, distort, dx, dy + a);
+    carpet(levels - 1, a, distort, dx + a, dy + a);
+  }
+  else
+  {
+    float shiftX = static_cast<float>(rand()) / (static_cast<float>(RAND_MAX / distort)) - distort / 2;
+    float shiftY = static_cast<float>(rand()) / (static_cast<float>(RAND_MAX / distort)) - distort / 2;
+    rect(dx - shiftX, dy - shiftY, a);
+    glFlush();
+    int xd = 2;
+  }
+}
+
+struct CarpetLevelData
+{
+  int level;
+  float a;
+  float dx = 0;
+  float dy = 0;
+};
+
+void carpetIt(int levels, float a, float distort = 0)
+{
+  queue<CarpetLevelData> q = queue<CarpetLevelData>();
+  q.push({levels, a});
+
+  while (!q.empty())
+  {
+    CarpetLevelData data = q.front();
+
+    if (data.level > 0)
+    {
+      data.a = data.a / 3;
+      q.push({data.level - 1, data.a, data.dx - data.a, data.dy - data.a});
+      q.push({data.level - 1, data.a, data.dx, data.dy - data.a});
+      q.push({data.level - 1, data.a, data.dx + data.a, data.dy - data.a});
+      q.push({data.level - 1, data.a, data.dx - data.a, data.dy});
+      q.push({data.level - 1, data.a, data.dx + data.a, data.dy});
+      q.push({data.level - 1, data.a, data.dx - data.a, data.dy + data.a});
+      q.push({data.level - 1, data.a, data.dx, data.dy + data.a});
+      q.push({data.level - 1, data.a, data.dx + data.a, data.dy + data.a});
+    }
+    else
+    {
+      float shiftX = static_cast<float>(rand()) / (static_cast<float>(RAND_MAX / distort)) - distort / 2;
+      float shiftY = static_cast<float>(rand()) / (static_cast<float>(RAND_MAX / distort)) - distort / 2;
+      rect(data.dx - shiftX, data.dy - shiftY, data.a);
+    }
+
+    q.pop();
+  }
+}
+
 //Drawing funciton
 void draw(void)
 {
   //Background color
-  glClearColor(0,1,0,1);
-  glClear(GL_COLOR_BUFFER_BIT );
+  glClearColor(0, 0, 0, 1);
+  glClear(GL_COLOR_BUFFER_BIT);
+
+  carpet(3, 1.8f, 0.05f);
+  //carpetIt(4, 1.8f, 0.05f);
+
   //Draw order
   glFlush();
 }
@@ -13,14 +116,14 @@ void draw(void)
 //Main program
 int main(int argc, char **argv)
 {
-  XD *xd = new XD();
+  srand(time(NULL));
   glutInit(&argc, argv);
-  
+
   //Simple buffer
-  glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB );
-  glutInitWindowPosition(50,25);
-  glutInitWindowSize(500,250);
-  glutCreateWindow("Green window");
+  glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+  glutInitWindowPosition(50, 25);
+  glutInitWindowSize(800, 800);
+  glutCreateWindow("Lab1");
 
   //Call to the drawing function
   glutDisplayFunc(draw);
